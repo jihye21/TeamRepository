5d170c905f9f49bbb091a669d064955fb02a088f
jihye21 <112855236+jihye21@users.noreply.github.com>
2024-11-20 15:15:45 +0900

    delete

--- chattingServer/src/chattingServer/KafkaWebSocketServer.java
+++ chattingServer/src/chattingServer/KafkaWebSocketServer.java
@@ -1,41 +1,68 @@
 package chattingServer;
 
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 public class KafkaWebSocketServer extends WebSocketServer{
-	public static void main(String [] args) {
+	List<WebSocket> connections = new ArrayList<WebSocket>();
+	public KafkaWebSocketServer(InetSocketAddress address) {
+		super(address);
+	}
+	public static void main(String [] args) {// 172.16.110.46
+		InetSocketAddress address = new InetSocketAddress("220.67.114.16", 1234);
+		KafkaWebSocketServer server = new KafkaWebSocketServer(address);
+		server.start();
 		
 	}
 
 	@Override
-	public void onClose(WebSocket arg0, int arg1, String arg2, boolean arg3) {
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("closed connection: " + conn.getRemoteSocketAddress());
+		connections.remove(conn);
 	}
 
 	@Override
-	public void onError(WebSocket arg0, Exception arg1) {
+	public void onError(WebSocket conn, Exception ex) {
 		// TODO Auto-generated method stub
-		
+		ex.printStackTrace();
 	}
 
 	@Override
-	public void onMessage(WebSocket arg0, String arg1) {
+	public void onMessage(WebSocket conn, String message) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Message from client: " + message);
+		broadcastMessage(conn, message);
 	}
 
 	@Override
-	public void onOpen(WebSocket arg0, ClientHandshake arg1) {
-		// TODO Auto-generated method stub
-		
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+		System.out.println("New Connecton: " + conn.getRemoteSocketAddress());
+		conn.send("welcom");
+		connections.add(conn);
 	}
 
 	@Override
 	public void onStart() {
 		// TODO Auto-generated method stub
+		System.out.println("WebSocket Server started");
+	}
+	
+	public void broadcastMessage(WebSocket conn, String message) {
+		for(WebSocket connection: connections) {
+			if(connection != conn) {
+				connection.send(conn.getRemoteSocketAddress()+ ": " + message);
+			}
+			
+		}
+	}
+	
+	public void startKafkaConsumer(WebSocket conn, String message) {
 		
 	}
 }
